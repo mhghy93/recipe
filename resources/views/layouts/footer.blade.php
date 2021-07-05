923f1d7cc07de8461d77252603065040cff9657b
mustaque <hussainmustaque98407@gmail.com>
2021-07-05 18:40:20 +0530

    footer added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer id="sticky-footer" class="py-5">
+    <div class="container text-center">
+        <small>Copyright &copy; IndoRecipes</small>
+    </div>
+</footer>
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -27,14 +27,14 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
+<body class="d-flex flex-column">
+    <div id="page-content">
         {{-- Navbar --}}
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <i class="fas fa-hamburger"></i>
-                    {{ config('app.name', 'Recipes') }}
+                    IndoRecipes
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -105,6 +105,7 @@
         <main class="py-4">
             @yield('content')
         </main>
+        @extends('layouts.footer')
     </div>
 </body>
 </html>
