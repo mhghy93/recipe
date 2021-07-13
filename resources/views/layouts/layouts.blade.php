5f2b7afbbed8d37f8759e721bd8a0b9def7b2b96
mustaque <hussainmustaque98407@gmail.com>
2021-07-13 12:20:25 +0530

    project title changed

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -99,7 +99,7 @@
                     28 -102 -3z"/>
                     </g>
                     </svg>
-                    IndoRecipes
+                    EasyRecipes
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
